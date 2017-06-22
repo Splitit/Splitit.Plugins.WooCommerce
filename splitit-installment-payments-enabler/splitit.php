989c677335e4bd0b1ba159d9ca462921b32b393d
sudhanshu <sudhanshu.dwivedi@sooryen.com>
2017-06-22 13:59:35 +0530

    APi changes

--- splitit-installment-payments-enabler/splitit.php
+++ splitit-installment-payments-enabler/splitit.php
@@ -15,14 +15,24 @@ if(is_admin()){
     error_reporting(0);
 }
 
-// function test(){
-//     global $wpdb; 
-//     $ipn = "23123670308455655541";
-//     $table_name = $wpdb->prefix . 'splitit_logs';
-//     $fetch_items = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM ".$table_name." WHERE ipn =".$ipn ), ARRAY_A );
-//     print_r($fetch_items);die;
-// }
-// add_action('init','test');
+function wpbo_get_woo_version_number() {
+        // If get_plugins() isn't available, require it
+    if ( ! function_exists( 'get_plugins' ) )
+        require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    
+        // Create the plugins folder and file variables
+    $plugin_folder = get_plugins( '/' . 'woocommerce' );
+    $plugin_file = 'woocommerce.php';
+    
+    // If the plugin version number is set, return it 
+    if ( isset( $plugin_folder[$plugin_file]['Version'] ) ) {
+        return $plugin_folder[$plugin_file]['Version'];
+
+    } else {
+    // Otherwise return null
+        return NULL;
+    }
+}
 
 
 add_action('plugins_loaded', 'init_splitit_method', 0);
