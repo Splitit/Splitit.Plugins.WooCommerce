831b925d961dd53b84895f2e8ea9d7efd43de214
manniwaliyan <manish@mobikasa.com>
2019-05-13 16:11:26 +0530

    version upgrade to 2.2.2

--- splitit-installment-payments-enabler/classes/splitit-api.php
+++ splitit-installment-payments-enabler/classes/splitit-api.php
@@ -52,7 +52,7 @@ class SplitIt_API {
         }
         $params = array('UserName' => $this->_username,
                          'Password' => $this->_password,
-                         'TouchPoint' => array("Code" =>"WooCommercePlugin","Version" => "2.2.1")
+                         'TouchPoint' => array("Code" =>"WooCommercePlugin","Version" => "2.2.2")
                          );
 
         try {
