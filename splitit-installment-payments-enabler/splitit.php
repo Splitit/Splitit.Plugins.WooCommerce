7e663d7c46bc5a28edcdfb9b7222c9a187163478
alexey <alexey.kovalkov@emma-sleep.com>
2021-01-05 12:06:32 +0300

    fix culture

--- splitit-installment-payments-enabler/splitit.php
+++ splitit-installment-payments-enabler/splitit.php
@@ -2019,7 +2019,7 @@ return $price . "<br/>" . $textToDisplay;
         {
             $apiKey = $this->get_option('splitit_api_terminal_key');
             $culture = str_replace('_', '-', get_locale());
-            $culture = $culture != 'pt-BR' ?: 'pt-PT';
+            $culture = $culture != 'pt-BR' ? $culture : 'pt-PT';
             $currencyCode = "USD";
             if (get_woocommerce_currency() != "") {
                 $currencyCode = get_woocommerce_currency();
